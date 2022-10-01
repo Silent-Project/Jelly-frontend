920d9fe376d786d329053207ea7150147a6096ce
Samagent97 <sammccarthy090@gmail.com>
2022-10-01 12:36:45 +0100

    feat:saving repo

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ import Cart from "./pages/cart/Cart.jsx";
 import RegisterPage from "./pages/register/RegisterPage";
 import CheckOut from "./pages/checkout/CheckOut";
 import ContactPage from './pages/contact/ContactPage'
+
 import Products from './pages/products/Products.jsx'
+import NavBar from "./components/navBar/NavBar";
+
+
 function App() {
   return (
     <>
       <section style={{ width: "100%" }}>
         <BrowserRouter>
-          <Routes>
+          
+          <Routes >
+
+
+           
+ <Route exact path='/nav' element={<NavBar/>} />
             <Route exact path={path.homepage} element={<Home />} />
+
+
             <Route exact path={path.loginPage} element={<LoginPage />} />
             <Route exact path={path.registerpage} element={<RegisterPage />} />
             <Route exact path={path.cart} element={<Cart />} />
@@ -23,12 +34,18 @@ function App() {
             <Route exact path={path.checkout} element={<CheckOut />} />
             <Route exact path={path.product} element={<Products />} />
 
-            <Route exact path="/contact" element={<ContactPage />} />
-            <Route exact path="/details" element={<ProductDetails/>} />
+            {/* <Route exact path="/contact" element={<ContactPage />} />
+
+            <Route exact path="/details" element={<ProductDetails/>} /> */}
             <Route exact path="*" element={<h1>404</h1>} />
+
+           
+
           </Routes>
         </BrowserRouter>
       </section>
+
+      
     </>
   );
 }
